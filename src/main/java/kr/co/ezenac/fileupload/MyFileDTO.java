4fcfd6ef0040d43f3969051bf290a8b63847cf4e
kimjinmin4535 <kimjinmin4535@gmail.com>
2022-07-16 16:13:09 +0900

    22

---
+++ src/main/java/kr/co/ezenac/fileupload/MyFileDTO.java
@@ -0,0 +1,91 @@
+package kr.co.ezenac.fileupload;
+
+/*
+ idx	number
+name	varchar2(50)
+title	varchar2(200)
+cate	varchar2(30)
+ofile	varchar2(100)
+sfile	varchar2(30)
+postdate	date * 
+ */
+public class MyFileDTO {
+
+	private String idx;
+	private String name;
+	private String title;
+	private String cate;
+	private String ofile;
+	private String sfile;
+	private String postdate;
+	public String getIdx() {
+		return idx;
+	}
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCate() {
+		return cate;
+	}
+	public void setCate(String cate) {
+		this.cate = cate;
+	}
+	public String getOfile() {
+		return ofile;
+	}
+	public void setOfile(String ofile) {
+		this.ofile = ofile;
+	}
+	public String getSfile() {
+		return sfile;
+	}
+	public void setSfile(String sfile) {
+		this.sfile = sfile;
+	}
+	public String getPostdate() {
+		return postdate;
+	}
+	public void setPostdate(String postdate) {
+		this.postdate = postdate;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
